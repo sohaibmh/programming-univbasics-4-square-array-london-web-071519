4f013b50d1c707639e6298464452a2b5f19ce777
Sohaib Mubashir <sohaibmh@yahoo.com>
2019-06-30 01:00:40 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,11 @@
 
 
 def square_array(array)
-  counter = 0
-  while array[counter] do puts array[counter] ** 2
-    counter += 1 end
+  index = 0
+  while array[index] do puts array[index] ** 2
+    index += 1 end
 end
 
-a = [3, 4]
+a = [1, 2, 3]
 
-square_array(a)
+puts square_array(a)
